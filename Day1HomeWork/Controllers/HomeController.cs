f50ce4b09cc7fc72092bd00457e4bf84a04cd022
SMsayhellogirl <sayhellogirlgn01260021@gmail.com>
2016-08-05 00:58:48 +0800

    完成homework

--- Day1HomeWork/Controllers/HomeController.cs
+++ Day1HomeWork/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Day1HomeWork.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -25,6 +26,64 @@ namespace Day1HomeWork.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        public ActionResult Day1homework()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Day1homework(Day1ViewMode day)
+        {
+            if (TempData["GetMyView"] == null)
+            {
+                TempData["GetMyView"] = getdefult();
+            }
+            Day1ViewMode viewdata = new Day1ViewMode();
+            viewdata.Dno = 4;
+            viewdata.Dtype = day.Dtype;
+            viewdata.Dmoney = day.Dmoney;
+            viewdata.Dday = day.Dday;
+            viewdata.Dnote = day.Dnote;
+
+            List<Day1ViewMode> myviewdata = new List<Day1ViewMode>();
+            myviewdata = (List<Day1ViewMode>)TempData["GetMyView"];
+            myviewdata.Add(viewdata);
+
+            TempData["GetMyView"] = myviewdata;
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Day1homeworkChirldaction()
+        {
+            if (TempData["GetMyView"] == null)
+            {
+                TempData["GetMyView"] = getdefult();
+            }
+
+            return View(TempData["GetMyView"]);
+        }
+
+        public List<Day1ViewMode> getdefult()
+        {
+            List<Day1ViewMode> myviewdata = new List<Day1ViewMode>();
+            
+
+            for (int i = 1; i < 4; i++)
+            {
+                Day1ViewMode myview = new Day1ViewMode();
+                myview.Dno = i;
+                myview.Dtype = "支出";
+                myview.Dday = "2016-01-0"+ i.ToString();
+                myview.Dmoney = (i * 150).ToString();
+                myview.Dnote = "我是備註" + i.ToString();
+                myviewdata.Add(myview);
+            }
+
+            return myviewdata;
         }
     }
 }
---
+++ Day1HomeWork/Models/Day1ViewMode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Day1HomeWork.Models
+{
+    public class Day1ViewMode
+    {
+        public int Dno { get; set; }
+
+        [Display(Name = "類別")]
+        public string Dtype { get; set; }
+
+        [Display(Name = "金額")]
+        public string Dmoney { get; set; }
+
+        [Display(Name = "日期")]
+        public string Dday { get; set; }
+
+        [Display(Name = "備註")]
+        public string Dnote { get; set; }
+    }
+}
